51d2ed95243e0fe835f58c004c050573dd23c74b
Zoran Ljubinkovic <zoranlj@gmail.com>
2018-09-18 23:31:34 +0200

    tileLayer change implemented

--- src/components/l-map/l-map.tsx
+++ src/components/l-map/l-map.tsx
@@ -13,7 +13,13 @@ import L from 'leaflet';
 export class LMap {
   @Element() LMapHTMLElement: HTMLElement;
   @Prop() iconUrl: string;
-  @Prop() tileLayer: string;
+  @Prop() tileLayerUrl: string;
+  @Watch('tileLayerUrl')
+  handleLTileLayerChanged(tileLayerUrl: string) {
+    console.log('l-map handleLTileLayerChanged');
+    console.log('l-map tileLayerUrl', tileLayerUrl);
+    this.tileLayer.setUrl(tileLayerUrl);
+  }
   @Prop() locations: string;
   @Prop() center: string;
   @Prop() zoom: string;
@@ -27,6 +33,7 @@ export class LMap {
   @Event() message: EventEmitter;
 
   LMap;
+  tileLayer;
 
   render() {
     return (
@@ -36,7 +43,7 @@ export class LMap {
 
   componentDidLoad() {
     console.log('l-map componentDidLoad');
-    console.log('l-map tilelayer', this.tileLayer);
+    console.log('l-map tileLayerUrl', this.tileLayerUrl);
     console.log('l-map iconurl', this.iconUrl);
     console.log('l-map locations', this.locations);
     console.log('l-map center', this.center);
@@ -44,10 +51,12 @@ export class LMap {
     console.log('l-map min zoom', this.minZoom);
     console.log('l-map max zoom', this.maxZoom);
     const LMapElement: HTMLElement = this.LMapHTMLElement.shadowRoot.querySelector('#l-map');
+
     this.LMap = L.map(LMapElement, { minZoom: Number(this.minZoom), maxZoom: Number(this.maxZoom), maxBounds: [[-90, -180],[90, 180]]})
       .setView(JSON.parse(this.center), Number(this.zoom));
-    const tilelayer = L.tileLayer(this.tileLayer);
-    tilelayer.addTo(this.LMap);
+
+    this.tileLayer = L.tileLayer(this.tileLayerUrl);
+    this.tileLayer.addTo(this.LMap);
 
     this.LMap.on('click', (e:any) => {
       console.log('l-map component send location message');
