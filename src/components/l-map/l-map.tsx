bd1f759e9db3e4289a9abd76b344e2626537ca40
Zoran Ljubinkovic <zoranlj@gmail.com>
2018-09-22 22:36:40 +0200

    layer control added

--- src/components/l-map/l-map.tsx
+++ src/components/l-map/l-map.tsx
@@ -1,6 +1,7 @@
 import { Component, Prop, Element, Watch, Event, EventEmitter } from '@stencil/core';
-import leaflet from 'leaflet';
-// import esri from'../../../node_modules/esri-leaflet/dist/esri-leaflet'
+import L from 'leaflet';
+import esri from'../../../node_modules/esri-leaflet/dist/esri-leaflet';
+L.esri = esri;
 
 @Component({
   tag: 'l-map',
@@ -15,17 +16,11 @@ export class LMap {
   @Element() LMapHTMLElement: HTMLElement;
   @Prop() iconUrl: string;
   @Prop() tileLayerUrl: string;
-  @Watch('tileLayerUrl')
-  handleLTileLayerChanged(tileLayerUrl: string) {
-    console.log('l-map handleLTileLayerChanged');
-    console.log('l-map tileLayerUrl', tileLayerUrl);
-    this.tileLayer.setUrl(tileLayerUrl);
-  }
-  @Prop() locations: string;
   @Prop() center: string;
   @Prop() zoom: string;
   @Prop() minZoom: string;
   @Prop() maxZoom: string;
+  @Prop() locations: string;
   @Watch('locations')
   handleLocationsChanged(locations: string) {
     console.log('l-map handleLocationsChanged');
@@ -34,7 +29,8 @@ export class LMap {
   @Event() message: EventEmitter;
 
   LMap;
-  tileLayer;
+  layerGroupTiles = L.layerGroup();
+  layerGroupLocations = L.layerGroup();
 
   render() {
     return (
@@ -51,40 +47,58 @@ export class LMap {
     console.log('l-map zoom', this.zoom);
     console.log('l-map min zoom', this.minZoom);
     console.log('l-map max zoom', this.maxZoom);
+
     const LMapElement: HTMLElement = this.LMapHTMLElement.shadowRoot.querySelector('#l-map');
 
-    this.LMap = leaflet.map(LMapElement, { zoomControl: false, minZoom: Number(this.minZoom), maxZoom: Number(this.maxZoom), maxBounds: [[-90, -180],[90, 180]]})
-      .setView(JSON.parse(this.center), Number(this.zoom));
+    const tileLayer = L.tileLayer(this.tileLayerUrl).addTo(this.layerGroupTiles);
+    const esriNationalGeographic = L.esri.basemapLayer('NationalGeographic').addTo(this.layerGroupTiles);
 
-    this.tileLayer = leaflet.tileLayer(this.tileLayerUrl);
-    this.tileLayer.addTo(this.LMap);
+    if (this.locations.length) {
+      this.addMarkers(JSON.parse(this.locations));
+    }
 
-    this.LMap.on('click', (e:any) => {
-      console.log('l-map component send location message');
-      this.message.emit(e.latlng.lat + ", " + e.latlng.lng);
+    let esriFeatureLayerStates = L.esri.featureLayer({
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_States_Generalized/FeatureServer/0',
+      style: function () {
+        return { color: '#545454', weight: 1 };
+      },
+      useCors: false
     });
-    this.addMarkers(JSON.parse(this.locations));
-    //
-    // esri.basemapLayer('Gray').addTo(this.LMap);
-    // esri.basemapLayer('Topographic').addTo(this.LMap);
-    //
-    // esri.featureLayer({
-    //   url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/3",
-    //   style: function () {
-    //     return {color: '#bada55', weight: 2 };
-    //   }
-    // }).addTo(this.LMap);
+
+    this.LMap = L.map(LMapElement, {
+      zoomControl: false,
+      minZoom: Number(this.minZoom),
+      maxZoom: Number(this.maxZoom),
+      maxBounds: [[-90, -180],[90, 180]],
+      layers: [this.layerGroupTiles, this.layerGroupLocations, esriFeatureLayerStates],
+    })
+      .setView(JSON.parse(this.center), Number(this.zoom))
+      .on('click', (e:any) => {
+          console.log('l-map component send location message');
+          this.message.emit(e.latlng.lat + ', ' + e.latlng.lng);
+        });
+
+    const baseMaps = {
+      'Custom Tile Layer': tileLayer,
+      'Esri National Geographic': esriNationalGeographic
+    };
+
+    const overlayMaps = {
+      'Custom Locations': this.layerGroupLocations,
+      'Esri States': esriFeatureLayerStates
+    };
+
+    L.control.layers(baseMaps, overlayMaps).addTo(this.LMap);
+
   }
 
   addMarkers(locations) {
-    const modusLogo = leaflet.icon({
+    const modusLogo = L.icon({
       iconUrl: this.iconUrl,
       iconSize: [30, 30]
     });
-    let marker;
     locations.map(latLng => {
-      marker = leaflet.marker(latLng, { icon: modusLogo });
-      marker.addTo(this.LMap);
+      L.marker(latLng, { icon: modusLogo }).addTo(this.layerGroupLocations);
     });
   }
 
